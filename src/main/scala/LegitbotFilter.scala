e2935e6cad824a91e4d3a0fb7a71daad172fd816
Alexander Azarov <azarov@osinka.ru>
2015-11-13 13:02:41 +0300

    No need in `User-Agent` in the filter

--- src/main/scala/LegitbotFilter.scala
+++ src/main/scala/LegitbotFilter.scala
@@ -8,21 +8,20 @@ import play.api.mvc._
 case class LegitbotFilter(errorHandler: RequestHeader => Result = Defaults.errorHandler, bots: Seq[VerifiableBot] = Defaults.bots) extends Filter {
   private val logger = LoggerFactory.getLogger(getClass)
 
-  def apply(nextFilter: (RequestHeader) => Future[Result])(requestHeader: RequestHeader): Future[Result] =
-    requestHeader.headers.get(HeaderNames.USER_AGENT) flatMap { userAgent =>
-      val malicious =
-        bots filter { _.isDefinedAt(requestHeader) } collectFirst {
-          case bot if bot.apply(requestHeader) =>
-            logger.debug(s"${requestHeader.remoteAddress} $requestHeader matches ${bot.getBotName}, and it's valid.")
-            false
-          case bot =>
-            logger.info(s"${requestHeader.remoteAddress} $requestHeader pretends to be ${bot.getBotName}, but it's not.")
-            true
-        }
+  def apply(nextFilter: (RequestHeader) => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
+    val malicious =
+      bots filter { _.isDefinedAt(requestHeader) } collectFirst {
+        case bot if bot.apply(requestHeader) =>
+          logger.debug(s"${requestHeader.remoteAddress} $requestHeader matches ${bot.getBotName}, and it's valid.")
+          false
+        case bot =>
+          logger.info(s"${requestHeader.remoteAddress} $requestHeader pretends to be ${bot.getBotName}, but it's not.")
+          true
+      }
 
-      if (malicious.exists(true.==)) Some(Future successful errorHandler(requestHeader))
-      else None
-    } getOrElse nextFilter(requestHeader)
+    if (malicious.exists(true.==)) Future successful errorHandler(requestHeader)
+    else nextFilter(requestHeader)
+  }
 }
 
 object LegitbotFilter extends LegitbotFilter(Defaults.errorHandler, Defaults.bots)
